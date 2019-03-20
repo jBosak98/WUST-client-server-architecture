932c97a1b689e9648f5475fcd4d5362be15d77a6
jBosak98 <you@example.com>
2019-03-20 19:03:31 +0100

    auth

--- bank(csa2)/bankServer/src/main/kotlin/com/jbosak/csa/bank/server/main.kt
+++ bank(csa2)/bankServer/src/main/kotlin/com/jbosak/csa/bank/server/main.kt
@@ -2,30 +2,29 @@ package com.jbosak.csa.bank.server
 
 
 
+import org.apache.xmlrpc.common.XmlRpcHttpRequestConfig
+import org.apache.xmlrpc.server.AbstractReflectiveHandlerMapping.AuthenticationHandler
 import org.apache.xmlrpc.server.PropertyHandlerMapping
 import org.apache.xmlrpc.server.XmlRpcServerConfigImpl
 import org.apache.xmlrpc.webserver.WebServer
 
+const val SERVER_PORT = 5059
 
-
-
-val SERVER_PORT = 5059
 fun main(){
     print("xd")
-
-
-
-    val myServlet = MyServlet()
-
-
-
 
     val webServer = WebServer(5059)
     val xmlRpcServer = webServer.xmlRpcServer
 
     val phm = PropertyHandlerMapping()
 
-    phm.addHandler("Bank", SessionManager()::class.java)
+    val sessionManager = SessionManager()
+    phm.authenticationHandler = AuthenticationHandler {
+        val config = it.config as XmlRpcHttpRequestConfig
+        return@AuthenticationHandler isAuthenticated(config.basicUserName, config.basicPassword, it.methodName)
+    }
+
+    phm.addHandler("Bank", sessionManager::class.java)
     xmlRpcServer.handlerMapping = phm
     val config: XmlRpcServerConfigImpl = xmlRpcServer.config as XmlRpcServerConfigImpl
     config.isEnabledForExceptions = true
@@ -33,21 +32,28 @@ fun main(){
     config.isEnabledForExtensions = true
     println(config.isEnabledForExceptions)
     xmlRpcServer.setConfig(config)
+    println(phm.javaClass)
+    println(phm.getMethodSignature(phm.listMethods[1]))
+
+    phm.listMethods.forEach { println(it) }
 
     webServer.start()
 
     System.out.println("Server started successfully");
     System.out.println("Listening on port: $SERVER_PORT")
 
-//    val x : Class<BankServer> = BankServer::class.java
-//    println(x.classes)
-//    x.declaredMethods.forEach {
-//        println(it.genericReturnType)
-//        println(it.returnType)
-//        it.parameters.forEach {
-//            println(" ${it.type}   ${it.declaringExecutable}")
-//        }
-//        println("")
-//        println()
-//    }
+}
+
+fun isAuthenticated(basicUserName: String?, basicPassword: String?, methodName: String): Boolean {
+    println(methodName)
+    println("$basicUserName $basicPassword")
+    if (methods.containsKey(methodName) && !methods[methodName]!!){
+        return true
+    }
+    users.forEach {
+        if (it.password == basicPassword && it.username == basicUserName){
+            return  true
+        }
+    }
+    return false
 }
