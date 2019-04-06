c9154382cbde03ad6b2aca42f5bb860220c6ee05
jBosak98 <jbosak98@gmail.com>
2019-04-06 14:26:28 +0200

    fifo case when we have free space but data is already in memory fixed

--- RMI(csa4)/pageReplacement/src/main/kotlin/FifoImpl.kt
+++ RMI(csa4)/pageReplacement/src/main/kotlin/FifoImpl.kt
@@ -1,27 +1,29 @@
 import model.Page
 import java.rmi.server.UnicastRemoteObject
 
-class FifoImpl : UnicastRemoteObject(),
-    Fifo {
+class FifoImpl : UnicastRemoteObject(), Fifo {
     override fun run(sizeOfMemory: Int,  pages: ArrayList<Page>): Int{
         var faultCounter = 0
         var memory: ArrayList<Page> = arrayListOf()
         var eldest = 0
         var pageCounter = 0
         while (pageCounter < pages.size){
-
-            if (memory.isEmpty() || memory.size < sizeOfMemory){
+            if(memory.isEmpty()){
                 memory.add(pages[pageCounter])
                 faultCounter++
-            }
-            else if (isInMemory(pages[pageCounter], memory).not()) {
-                memory.mapIndexed { index, it ->
-                    if (it.timeInMemory > memory[eldest].timeInMemory) eldest = index
+            }else if (isInMemory(pages[pageCounter], memory).not()) {
+                if (memory.size < sizeOfMemory){
+                    memory.add(pages[pageCounter])
+                    faultCounter++
+                }else {
+                    memory.mapIndexed { index, it ->
+                        if (it.timeInMemory > memory[eldest].timeInMemory) eldest = index
+                    }
+                    faultCounter++
+                    memory[eldest] = pages[pageCounter]
                 }
-                faultCounter++
-                memory[eldest] = pages[pageCounter]
-            }
 
+            }
             pageCounter++
             memory.forEach { it.timeInMemory++ }
         }
