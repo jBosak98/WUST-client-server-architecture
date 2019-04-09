b34026bbb854e254f471650c95ba84aa21f8d874
jBosak98 <jbosak98@gmail.com>
2019-04-09 20:04:29 +0200

    OPT algorithm in client

--- RMI(csa4)/PageReplacementSymulatorClient/src/main/kotlin/PageController.kt
+++ RMI(csa4)/PageReplacementSymulatorClient/src/main/kotlin/PageController.kt
@@ -1,3 +1,5 @@
+import algorithm.Fifo
+import algorithm.Opt
 import javafx.collections.FXCollections
 import model.Page
 import tornadofx.Controller
@@ -6,12 +8,15 @@ import java.rmi.registry.Registry
 
 class PageController: Controller(){
     var fifo: Fifo? = null
+    var opt: Opt? = null
     var pages = FXCollections.observableArrayList<Page>()
 
 
     init {
         val reg: Registry = LocateRegistry.getRegistry(1097)
         fifo =  reg.lookup("//localhost/fifo") as Fifo
+        opt = reg.lookup("//localhost/opt") as Opt
+
 
     }
     fun save(inputValue: Int){
@@ -25,9 +30,11 @@ class PageController: Controller(){
             val fifoPages: ArrayList<Page> = arrayListOf()
             pages.forEach { fifoPages.add(Page(it.data)) }
 
-             val faults = fifo?.run(sizeOfMemory,  fifoPages)
+             val fifoFaults = fifo?.run(sizeOfMemory,  fifoPages)
+             val optFaults = opt?.run(sizeOfMemory,  fifoPages)
 
-            println("faults: $faults")
+            println("FIFO faults: $fifoFaults")
+            println("OPT faults: $optFaults")
 
         }catch (e: Exception){
             println("Remote call error")
